23311b1100239244d658c1a64a14e73d72a60084
argonoid48 <158807187+argonoid48@users.noreply.github.com>
2024-10-18 14:45:57 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+# Галкин Андрей
+# Домашняя работа по уроку "Условная конструкция. Операторы if, elif, else"
+
+first = input('Введите первое число: ')
+second = input('Введите второе число: ')
+third = input('Введите третие число: ')
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
